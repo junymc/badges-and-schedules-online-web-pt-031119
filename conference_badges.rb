f8321aea2eb0edc75c99d9a4af051ce024f3080c
Junyoung McArdle <juny.mcardle@gmail.com>
2019-03-19 03:05:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,7 @@ def batch_badge_creator(names)
   end
   array
 end
+
+def assign_rooms(speakers)
+  speakers.each_with_index do |speaker, index|
+    puts "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
